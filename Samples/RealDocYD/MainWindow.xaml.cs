b5a611d9892162914049021b8eaee60e47eb272b
Evgeny Redikultsev <effect-project@yandex.ru>
2019-06-27 14:49:14 +0500

    Выложил файлы

---
+++ Samples/RealDocYD/MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Disk.SDK;
+using Disk.SDK.Provider;
+
+namespace RealDocYD
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public class IssueModel
+    {
+        public int SiteNum { get; set; }
+        public int IssueNum { get; set; }
+    }
+    public partial class MainWindow : Window
+    {
+        private const string CLIENT_ID = "f75503b1cc734370a0a0e1f6e1acabe5";
+        private const string constAccessToken = "AgAAAAACuFN0AAW808LC4Pxn90nevCsZgQSS4Oo";
+
+        private string currentPath;
+
+        private IDiskSdkClient sdk;
+        private ObservableCollection<DiskItemInfo> folderItems;
+        private readonly ICollection<DiskItemInfo> selectedItems = new Collection<DiskItemInfo>();
+        private readonly ICollection<DiskItemInfo> cutItems = new Collection<DiskItemInfo>();
+        private readonly ICollection<DiskItemInfo> copyItems = new Collection<DiskItemInfo>();
+        private bool isLaunch;
+
+        public static string AccessToken { get; set; }
+
+        public IssueModel Issue { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Issue = new IssueModel();
+            this.DataContext = Issue;
+            this.CreateSdkClient();
+        }
+
+        private void btnPublish_Click(object sender, RoutedEventArgs e)
+        {
+            string s;
+            string siteNum = Convert.ToString(this.Issue.SiteNum);
+            string issueNum = Convert.ToString(this.Issue.IssueNum);
+
+            try
+            {
+                tblPath.Text = "Определение ссылки...";
+                this.ShowProgress();
+                currentPath = "wk/IP_R/" + siteNum + "/ISSUEDS/" + issueNum + "/";
+                this.sdk.PublishAsync(this.currentPath);
+                s = "Объект: "+ siteNum + ", Выпуск: " + issueNum;
+            }
+            catch
+            {
+                s = "Ошибка публикации";     
+            }
+            tblPath.Text = s;
+
+        }
+
+        private void CreateSdkClient()
+        {
+            this.sdk = new DiskSdkClient();
+            sdk.AccessToken = constAccessToken;
+            this.AddCompletedHandlers();
+        }
+
+        private void unPublish_Click(object sender, RoutedEventArgs e)
+        {
+            //this.ShowProgress();
+            //this.sdk.UnpublishAsync(this.selectedItems.First().OriginalFullPath);
+        }
+
+        private void SdkOnPublishCompleted(object sender, GenericSdkEventArgs<string> e)
+        {
+            if (e.Error == null)
+            {
+                this.Dispatcher.BeginInvoke(
+                    new Action(
+                        () =>
+                        {
+                            //this.popupLink.IsOpen = true;
+                            if (e.Result == null || e.Result == "") tblLink.Text = "Ошибка поиска папки!";
+                            else
+                            {
+                                tblLink.Text = "Ссылка на папку скопирована в буфер обмена";
+                                Clipboard.SetText(e.Result);
+                            }
+                            
+                        }));
+                //this.InitFolder(this.currentPath);
+            }
+            else
+            {
+                //this.ProcessError(e.Error);
+            }
+
+            this.ChangeVisibilityOfProgressBar(Visibility.Collapsed);
+        }
+
+        private void AddCompletedHandlers()
+        {
+            //this.sdk.CopyCompleted += this.SdkOnCopyCompleted;
+            //this.sdk.GetListCompleted += this.SdkOnGetListCompleted;
+            //this.sdk.MakeFolderCompleted += this.SdkOnMakeFolderCompleted;
+            //this.sdk.MoveCompleted += this.SdkOnMoveCompleted;
+            this.sdk.PublishCompleted += this.SdkOnPublishCompleted;
+            //this.sdk.RemoveCompleted += this.SdkOnRemoveCompleted;
+            //this.sdk.TrashCompleted += this.SdkOnTrashCompleted;
+            //this.sdk.UnpublishCompleted += this.SdkOnUnpublishCompleted;
+        }
+
+        private void ChangeVisibilityOfProgressBar(Visibility visibility, bool isIndeterminate = true)
+        {
+            this.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                this.pbWaiting.Value = 0;
+                this.pbWaiting.Visibility = visibility;
+                this.pbWaiting.IsIndeterminate = isIndeterminate;
+            }));
+        }
+
+        private void ShowProgress(bool isIndeterminate = true)
+        {
+            this.pbWaiting.Visibility = Visibility.Visible;
+            this.pbWaiting.IsIndeterminate = isIndeterminate;
+        }
+
+        private void UpdateProgress(ulong current, ulong total)
+        {
+            this.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                this.pbWaiting.Value = current;
+                this.pbWaiting.Maximum = total;
+            }));
+        }
+    }
+}
---
+++ Samples/SdkSample.WPF/LoginWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Windows;
+
+using Disk.SDK;
+using Disk.SDK.Provider;
+
+namespace SdkSample.WPF
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        /// TODO: Register your application https://oauth.yandex.ru/client/new and set here your application ID and return URL.
+
+//#error define your client_id and return_url
+        private const string CLIENT_ID = "f75503b1cc734370a0a0e1f6e1acabe5";
+        //private const string RETURN_URL = "https://oauth.yandex.ru/authorize?response_type=token&client_id=f75503b1cc734370a0a0e1f6e1acabe5";
+        //private const string RETURN_URL = "https://oauth.yandex.ru/verification_code";
+        private const string RETURN_URL = "https://oauth.yandex.ru/verification_code#access_token=AgAAAAACuFN0AAW808LC4Pxn90nevCsZgQSS4Oo&token_type=bearer&expires_in=31524949";
+
+        private readonly IDiskSdkClient sdkClient;
+
+        public LoginWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        public LoginWindow(IDiskSdkClient sdkClient)
+            : this()
+        {
+            this.sdkClient = sdkClient;
+            //Добавляем токен
+            this.sdkClient.AccessToken= "AgAAAAACuFN0AAW808LC4Pxn90nevCsZgQSS4Oo";
+            this.sdkClient.AuthorizeAsync(new WebBrowserWrapper(browser), CLIENT_ID, RETURN_URL, this.CompleteCallback);
+        }
+
+        private void CompleteCallback(object sender, GenericSdkEventArgs<string> e)
+        {
+            if (this.AuthCompleted != null)
+            {
+                this.AuthCompleted(this, new GenericSdkEventArgs<string>(e.Result));
+            }
+
+            this.Close();
+        }
+
+        public event EventHandler<GenericSdkEventArgs<string>> AuthCompleted;
+    }
+}
